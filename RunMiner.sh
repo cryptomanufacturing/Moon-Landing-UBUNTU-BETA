dce885e52ae1e17dc640609dd56413fcc8b662b0
Gary Bard <36722451+cryptomanufacturing@users.noreply.github.com>
2018-07-10 21:55:15 -0400

    Update RunMiner.sh

--- RunMiner.sh
+++ RunMiner.sh
@@ -1,2 +1,2 @@
 # Script to Launch Miner Program
-gnome-terminal -- ./SmartMiner.v3.1 -cd 0 1 2 -l 99.255.106.25:3333 -u 0xa0354a4ca1ded889fd3ae845d0cd16ecf97a17f927675add64bf6dca5bbd3dce
+gnome-terminal -- ./SmartMiner.v3.1 -cd 0 1 2 3 4 5 -l 99.255.106.25:3333 -u 0xa0354a4ca1ded889fd3ae845d0cd16ecf97a17f927675add64bf6dca5bbd3dce
